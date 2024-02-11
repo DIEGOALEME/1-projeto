69c9d236f327762639d789b19ef8d46d67f84abd
DIEGOALEME <diegoaleme@gmail.com>
2024-02-11 13:26:28 -0300

    Mario

--- scripts.js
+++ scripts.js
@@ -7,15 +7,11 @@ const telamascara = document.querySelector(".mascara-formulario")
 function mostrarform() {
     forme.style.left = "50%"
     forme.style.transform = "translatex(-50%)"
-    mascara.style.visibility = "visible"
-
-
-
-}
+    mascara.style.visibility = "visible"}
+    
 function esconderform() {
     forme.style.left = "-300%"
     forme.style.transform = "translatex(0)"
-    mascara.style.visibility = "hidden"
-}
+    mascara.style.visibility = "hidden"}
 
 
