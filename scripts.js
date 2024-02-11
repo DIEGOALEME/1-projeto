bafd70ace30987774b3781dcfe3ccb3e8b35fed9
DIEGOALEME <diegoaleme@gmail.com>
2024-02-10 21:31:37 -0300

    1 projeto

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const forme = document.querySelector(".fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+const telamascara = document.querySelector(".mascara-formulario")
+
+
+
+function mostrarform() {
+    forme.style.left = "50%"
+    forme.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+
+
+
+}
+function esconderform() {
+    forme.style.left = "-300%"
+    forme.style.transform = "translatex(0)"
+    mascara.style.visibility = "hidden"
+}
+
+
